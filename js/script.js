3ebed5c00b37333d1105a8166a16b73e44e97433
Owlomir <rl666130386@gmail.com>
2021-10-15 16:21:33 +0300

    add an extended type

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 // НЕ СТАВТЕ 2 ПОЖАЛУЙСТА, Я СТАРАЛСЯ
 
-const btn = document.getElementById('countBtn')
-const resultDisplay = document.getElementById('resultDisplay')
 
-btn.addEventListener('click', division)
+const countBtn = document.getElementById('countBtn')
+const moreBtn = document.getElementById('moreBtn')
+const resultDisplay = document.getElementById('resultDisplay')
+const moreResult = document.getElementById('more-result')
+const moreBlock = document.getElementById('more-conteiner')
+const rangeValue = document.getElementById('rangeValue').value
+const rangeValueDisplay = document.getElementById('rangeValueDisplay')
+let moreLever = [0, 1]
+
+countBtn.addEventListener('click', division)
+moreBtn.addEventListener('click', displayMore)
+
 
 function division() {
     let firstNumber = Number(document.getElementById('firstNumber').value)
@@ -15,9 +24,9 @@ function division() {
 
     answer.push(`${dec2bin(intermediateResult)}.`)
 
-    for(let i = 0; i < 7; i++) {
+    for(let i = 0; i < 20; i++) {
         console.log(currentNumber);
-        if(currentNumber >= 0) {
+        if(currentNumber >= 0) {        
             answer.push('1')
             currentNumber = currentNumber * 2 - secondNumber
         } else {
@@ -25,9 +34,42 @@ function division() {
             currentNumber = currentNumber * 2 + secondNumber
        }
     }
+
     answer.splice(1, 1)
     console.log(answer);
-    resultDisplay.innerText = answer.join('')
+    answer = Number(answer.join(''))
+    console.log(answer)
+    resultDisplay.innerText = answer.toFixed(6)
+
+
+    if(moreLever[0] == 1) {
+        moreResult.innerText = answer.toFixed(moreRangeChanger())
+    }
+    console.log(answer.toFixed(16))
+
+}
+
+function displayMore() {
+    moreLever.reverse()
+    console.log(moreLever)
+    
+    if(moreLever[0] == 0) { 
+        moreBlock.classList.remove('display')
+        moreBlock.classList.add('more-conteiner')
+        moreBtn.classList.remove('moreBtn-active')
+    } else {
+        moreBlock.classList.add('more-conteiner')
+        moreBlock.classList.add('display')
+        moreBtn.classList.add('moreBtn-active')
+    }
+    
+}
+
+function moreRangeChanger() {
+    const rangeValue = document.getElementById('rangeValue').value
+    rangeValueDisplay.innerText = rangeValue
+    console.log(rangeValue)
+    return rangeValue
 }
 
 function dec2bin(dec){
